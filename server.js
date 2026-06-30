8b6d5b0fdeb92fbe01ec53a1312dbe25c3a29694
MrY <yoseefy716.nabegh@gmail.com>
2026-06-30 16:23:00 +0300

    test

--- server.js
+++ server.js
@@ -1,26 +1,10 @@
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-const io = require("socket.io")(server, {
-    cors: { origin: "*" }
-});
-
-// Render will provide the PORT
+const io = require("socket.io")(server, { cors: { origin: "*" } });
 const PORT = process.env.PORT || 3000;
 
 io.on('connection', (socket) => {
-    console.log('Device connected:', socket.id);
-
-    // Relay signaling data between peers
-    socket.on('signal', (data) => {
-        socket.broadcast.emit('signal', data);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('Device disconnected');
-    });
+    socket.on('signal', (data) => socket.broadcast.emit('signal', data));
 });
-
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+server.listen(PORT);
